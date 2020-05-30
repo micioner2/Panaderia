19e9393f7934c3572c9c1eb00d10c36bc8bc8ca2
micioner2 <mauricio_pella1@hotmail.com>
2020-05-30 14:13:51 -0500

    first commit

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+let btnMenu = document.getElementById('btnMenu');
+let menu = document.getElementById('menu');
+btnMenu.addEventListener('click',function(){
+    'use stricit';
+    menu.classList.toggle('mostrar');
+})
+
